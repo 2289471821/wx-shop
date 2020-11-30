3593c8e733be5a29d83cbb72b9afe9c95f9da79d
wei <2289471821@qq.com>
2020-11-30 22:40:17 +0800

    collect

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -20,32 +20,39 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsObj: {}
+    goodsObj: {},
+    // 商品是否被收藏
+    isCollect: false
   },
   GoodsInfo: [],
 
   /**
-   * 生命周期函数--监听页面加载
+   * 生命周期函数--监听页面显示
    */
-  onLoad: function (options) {
-    const { goods_id } = options
+  onShow: function () {
+    let pages = getCurrentPages();
+    const { goods_id } = pages[pages.length-1].options
     this.getGoodsDetail(goods_id)
   },
 
   // 获取商品的详情数据
   async getGoodsDetail(goods_id) {
-    const { data: res } = await request({
-      url: '/goods/detail',
-      data: { goods_id }
-    })
+    const { data: res } = await request({ url: '/goods/detail', data: { goods_id } })
     this.GoodsInfo = res.message
+
+    // 获取缓存中的商品收藏数组数据
+    const collect = wx.getStorageSync('collect') || []
+    // 判断当前商品是否被收藏
+    let isCollect = collect.some(v => v.goods_id === this.GoodsInfo.goods_id)
+
     this.setData({ 
       goodsObj: {
         goods_name: res.message.goods_name,
         goods_price: res.message.goods_price,
         goods_introduce: res.message.goods_introduce,
         pics: res.message.pics,
-      }
+      },
+      isCollect
     })
   },
 
@@ -84,5 +91,35 @@ Page({
       icon: 'success',
       mask: false
     })
+  },
+
+  // 点击商品收藏或取消收藏
+  handleCollect() {
+    let isCollect = false
+    // 获取缓存中的商品收藏数组数据
+    let collect = wx.getStorageSync('collect') || []
+    // 判断当前商品是否被收藏
+    let index = collect.findIndex(v => v.goods_id === this.GoodsInfo.goods_id)
+    if(index!==-1) {
+      // 商品已被收藏，点击，取消收藏
+      collect.splice(index, 1)
+      wx.showToast({
+        title: '取消成功',
+        icon: 'success',
+        mask: true
+      })
+    }else {
+      // 商品未被收藏，点击收藏
+      collect.push(this.GoodsInfo)
+      isCollect = true
+      wx.showToast({
+        title: '收藏成功',
+        icon: 'success',
+        mask: true
+      })
+    }
+    // 把收藏数据重新存入缓存中
+    wx.setStorageSync('collect', collect)
+    this.setData({ isCollect })
   }
 })
--- pages/user/index.js
+++ pages/user/index.js
@@ -5,15 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    userInfo: {}
+    userInfo: {},
+    // 被收藏的商品数量
+    collectNumber: 0
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    // 获取缓存中的用户数据信息
     const userInfo = wx.getStorageSync("userInfo")
-    this.setData({ userInfo })
+    // 获取缓存中的收藏的商品数据
+    const collect = wx.getStorageSync("collect") || []
+    this.setData({ 
+      userInfo,
+      collectNumber: collect.length
+    })
   },
 
   handleGetUserInfo(e) {
